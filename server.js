e0bbb72dc2f6caba662d56621d173c35114afe8d
Paraboloid <paraboloid@protonmail.com>
2017-09-12 22:15:08 +0200

    Init

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(require('method-override')());
+app.use(session({ secret: 'conduit', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false  }));
 
 //--- End1
    
