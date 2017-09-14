a788cf91525895fd0b5ca42489809d301844ac74
Paraboloid <paraboloid@protonmail.com>
2017-09-14 22:01:23 +0200

    Init

--- server.js
+++ server.js
@@ -70,15 +70,48 @@ require('./config/passport');
 
 app.use(require('./routes'));
 
-
 app.get('/', function (req, res) {
     res.render('index.html', { pageCountMessage : null});
 });
 
-app.use(function(err, req, res, next){
-  console.error(err.stack);
-  res.status(500).send('Something bad happened!');
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (!isProduction) {
+  app.use(function(err, req, res, next) {
+    console.log(err.stack);
+
+    res.status(err.status || 500);
+
+    res.json({'errors': {
+      message: err.message,
+      error: err
+    }});
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({'errors': {
+    message: err.message,
+    error: {}
+  }});
+});
+
+//app.use(function(err, req, res, next){
+//  console.error(err.stack);
+//  res.status(500).send('Something bad happened!');
+//});
 
 app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
